553b23b709d0b428637295f49970c6c0c9d35e52
JazminDominguez <j.dgez.p@gmail.com>
2019-01-16 13:58:02 -0600

    la función vuelve el mensaje a ascii

--- src/index.js
+++ src/index.js
@@ -19,15 +19,28 @@
 const mssg = document.getElementById('mensaje1');
 // toma el dato obtenido del offset
 const offset = document.getElementById('offsetChoice');
-//el botón cifrar guarda los valores del mensaje y offset
+//el botón cifrar guarda los valores del mensaje y offset, abre función
 document.getElementById("codificar").addEventListener("click", () => {
   const mssgValue= mssg.value;
-  const offsetF = offset.value;
-  const mssgMayusculas= mssgValue.toUpperCase();
-  document.getElementById("mensajeUsuario").innerHTML = 'tu mensaje es ' + mssgMayusculas;
+  const offsetF = parseInt(offset.value);
   //volver el msj mayuscula
+  const mssgMayusculas= mssgValue.toUpperCase();
+  
+  //const ascii= mssgMayusculas.charCodeAt();
+
+//volver el mensaje a ASCII
+//nueva variable para el string vuelto ascii
+for (let i=0; i<mssgMayusculas.length; i++){
+    let ascii= mssgMayusculas.charCodeAt(i);
+    console.log(ascii);
+    
+    //let formula=(ascii=65+offsetF)%26+65;
+    //let resultado= formula
+   document.getElementById("mensajeUsuario").innerHTML = 'tu mensaje es ' +  ascii;
+}
+console.log (offsetF)});     
 
 
- console.log (offsetF, mssgMayusculas)});    
+   
 
 
