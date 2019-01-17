ca3293db66c9b56b8821a06645c2be3d2b4f1fbb
JazminDominguez <j.dgez.p@gmail.com>
2019-01-17 12:38:06 -0600

    test completos, funcion de encode y decode, sin css y html sencillo

--- src/cipher.js
+++ src/cipher.js
@@ -12,5 +12,17 @@ encode: (offsetF, mssgMayusculas) => {
     }
     return resultado
   },
-decode: () => {},
+decode: (offsetF2,mssgMayusculas2) => {
+    offsetF2 = parseInt(offsetF2)
+
+    let resultado2 = "";
+      for (let j=0; j < mssgMayusculas2.length; j++){ 
+      let ascii2   = mssgMayusculas2[j].charCodeAt();
+      let mssgAscii2 = (ascii2+65- offsetF2)%26+65;
+      let mssgCompleto2 = String.fromCharCode(mssgAscii2);
+      resultado2 += mssgCompleto2
+    }
+    return resultado2
+  
+}
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-//  --Eventos del DOM--
+//  --Eventos del DOM-- CIFRAR
 //guardar el mensaje desde el input
 const mssg = document.getElementById('mensaje1');
 // toma el dato obtenido del offset
 let offset = document.getElementById('offsetChoice');
 //el botón cifrar guarda los valores del mensaje y offset, abre función
-document.getElementById("codificar").addEventListener("click", () => {
+document.getElementById("code").addEventListener("click", () => {
   let mssgValue= mssg.value;
   let offsetF = offset.value;
   //volver el msj mayuscula
-  let mssgMayusculas= mssgValue.toUpperCase();
-window.cipher.encode(offsetF,mssgMayusculas)
-//document.getElementById("mensajeUsuario").innerHTML = 'tu mensaje es ' +  resultado;
-}
-)
-//console.log (offsetF)});     
+  let mssgMayusculas= mssgValue.toUpperCase()
+  window.cipher.encode(offsetF,mssgMayusculas)})   
+// -- DESCIFRAR
+const mssg2 = document.getElementById('mensaje2');
+let offset2= document.getElementById('offsetChoice2');
+document.getElementById("decode").addEventListener("click", () => {
+  let mssgValue2= mssg2.value;
+  let offsetF2 = offset2.value;
+  //volver el msj mayuscula
+  let mssgMayusculas2= mssgValue2.toUpperCase()
+  window.cipher.encode(offsetF2,mssgMayusculas2)})   
+
 
 
    
