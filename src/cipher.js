d7ea9add6cf5057b44bc26860e049af0d0db2168
JazminDominguez <j.dgez.p@gmail.com>
2019-01-21 23:19:20 -0600

    función code y encode, impresión en la pantalla

--- src/cipher.js
+++ src/cipher.js
@@ -6,7 +6,7 @@ encode: (offsetF, mssgMayusculas) => {
     let resultado = "";
       for (let i=0; i < mssgMayusculas.length; i++){ 
       let ascii   = mssgMayusculas[i].charCodeAt();
-      let mssgAscii = (ascii-65+ offsetF)%26+65;
+      let mssgAscii = (ascii-65+offsetF)%26+65;
       let mssgCompleto = String.fromCharCode(mssgAscii);
       resultado += mssgCompleto
     }
@@ -16,9 +16,9 @@ decode: (offsetF2,mssgMayusculas2) => {
     offsetF2 = parseInt(offsetF2)
 
     let resultado2 = "";
-      for (let j=0; j < mssgMayusculas2.length; j++){ 
-      let ascii2   = mssgMayusculas2[j].charCodeAt();
-      let mssgAscii2 = (ascii2+65- offsetF2)%26+65;
+      for (let i=0; i < mssgMayusculas2.length; i++){ 
+      let ascii2 = mssgMayusculas2[i].charCodeAt();
+      let mssgAscii2 = (ascii2+65-offsetF2)%26+65;
       let mssgCompleto2 = String.fromCharCode(mssgAscii2);
       resultado2 += mssgCompleto2
     }
--- src/index.js
+++ src/index.js
@@ -4,24 +4,26 @@ const mssg = document.getElementById('mensaje1');
 // toma el dato obtenido del offset
 let offset = document.getElementById('offsetChoice');
 //el botón cifrar guarda los valores del mensaje y offset, abre función
+const mssg2 = document.getElementById('mensaje2');
+let offset2= document.getElementById('offsetChoice2');
+
 document.getElementById("code").addEventListener("click", () => {
   let mssgValue= mssg.value;
   let offsetF = offset.value;
-  //volver el msj mayuscula
   let mssgMayusculas= mssgValue.toUpperCase()
-  window.cipher.encode(offsetF,mssgMayusculas)})   
+  const mensajeCifrado= window.cipher.encode(offsetF,mssgMayusculas);
+  mssg2.value = mensajeCifrado;
+  })
+
+
 // -- DESCIFRAR
-const mssg2 = document.getElementById('mensaje2');
-let offset2= document.getElementById('offsetChoice2');
+
 document.getElementById("decode").addEventListener("click", () => {
-  let mssgValue2= mssg2.value;
+  let mssgValue2= mssg2.value.toUpperCase();
   let offsetF2 = offset2.value;
-  //volver el msj mayuscula
   let mssgMayusculas2= mssgValue2.toUpperCase()
-  window.cipher.encode(offsetF2,mssgMayusculas2)})   
-
-
-
-   
+  const mensajeDescifrado= window.cipher.decode(offsetF2,mssgMayusculas2);
+  mssg.value = mensajeDescifrado;
+ })   
 
 
